ed7dcd4f35a51499f1cc6552fff9d32d645cf7ee
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-07-19 08:43:49 -0700

    tsk 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    cp = []
-    for x in range(len(my_list)):
-        if my_list[x] == search:
-            copy.append(replace)
-        else:
-            copy.append(my_list[x])
-    return cp
+    new_list = [item if item != search else replace for item in my_list]
+    return new_list
