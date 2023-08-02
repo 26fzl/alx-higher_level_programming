b101b01a9aeb95ebe898fc186b7478262e401c06
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-08-02 09:59:09 -0700

    task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        for _ in range(self.__position[1]):
+            print("")
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        result = ""
+        if self.__size != 0:
+            for _ in range(self.__position[1]):
+                result += "\n"
+
+        for _ in range(self.__size):
+            result += " " * self.__position[0]
+            result += "#" * self.__size
+            if _ != self.__size - 1:
+                result += "\n"
+        return result
