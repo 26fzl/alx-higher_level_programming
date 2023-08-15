77a9ed44a851f4e917a75976424e9f45a1c53a66
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-08-15 04:08:28 -0700

    task 0 1 2

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Read file function"""
+def read_file(filename=""):
+    """ function that writes a string to a text file
+    and returns the number of characters written """
+    with open(filename, encoding="utf-8") as f:
+        print(read_data, end="")
