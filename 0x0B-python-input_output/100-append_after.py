889a2a5644a779d1cf8f530e53f1b852158561c6
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-08-16 02:54:24 -0700

    task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Search and update"""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """function that inserts a line of text to a file,
     after each line containing a specific string"""
