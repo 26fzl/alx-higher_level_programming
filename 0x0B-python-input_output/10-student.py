78cde6e713e9e1856c6943b3fb2ef846f5dbd116
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-08-16 04:33:55 -0700

    tasks

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Student to JSON with filter"""
+""" Class to JSON """
 
 
 class Student:
-    """Write a class Student"""
+    """represent a class Student"""
 
      def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-    return self.__dict__
+ def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {key: self.__dict__[key]
+                for key in attrs if key in self.__dict__}
