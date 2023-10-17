2d1f4e8fc288bbf1aa42aadd25b8bb5399d787a8
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-10-17 04:47:16 -0700

    takssk

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,15 +18,15 @@ class Rectangle {
   }
 
   rotate () {
-    const aux = this.width;
-      this.width = this.height;
-      this.height = aux;
-   }
+     const aux = this.width;
+     this.width = this.height;
+     this.height = aux;
+  }
 
   double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
+      this.width *= 2;
+      this.height *= 2;
+   }
 }
 
 module.exports = Rectangle;
