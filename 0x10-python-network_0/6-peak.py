3981ea9eac2e314a3cc914da044b85db24eae979
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-11-02 04:19:34 -0700

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-""" Finds a peak in a list of unsorted integers
-"""
+""" Finds a peak """
 
 def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers(int): list of integers
+    Returns: peak of list_of_integers or None
+    """
     size = len(list_of_integers)
-    
+    mid_e = size
+    mids = size // 2
+
     if size == 0:
         return None
 
-    size_semi = size // 2
-    size_semi2 = size // 2
-    
     while True:
-        if size_semi < size - 1 and list_of_integers[size_semi] < list_of_integers[size_semi + 1]:
-            if size_semi2 == 0:
-                size_semi2 = 2
-            size_semi += size_semi2 // 2
-
-        elif size_semi2 > 0 and list_of_integers[size_semi] < list_of_integers[size_semi - 1]:
-            if size_semi2 == 0:
-                size_semi2 = 2
-            size_semi -= size_semi2 // 2
+        mid_e = mid_e // 2
+        if (mids < size - 1 and
+                list_of_integers[mids] < list_of_integers[mids + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mids = mids + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mids] < list_of_integers[mids - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mids = mids - mid_e // 2
         else:
-            return list_of_integers[size_semi]
+            return list_of_integers[mids]
