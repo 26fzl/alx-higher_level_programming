70c2e53cac5442256d79eecebb4f7762d94ec1ac
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-11-02 04:11:38 -0700

    tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,14 +3,23 @@
 """
 
 def find_peak(list_of_integers):
-    """def doc"""
-    if list_of_integers:
-        a = 0
-        b = len(list_of_integers) - 1
-        while a < b:
-            m = (a + b) // 2
-            if list_of_integers[m] > list_of_integers[m + 1]:
-                b = m
-            else:
-                a = m + 1
-        return list_of_integers[a]
+    size = len(list_of_integers)
+    
+    if size == 0:
+        return None
+
+    size_semi = size // 2
+    size_semi2 = size // 2
+    
+    while True:
+        if size_semi < size - 1 and list_of_integers[size_semi] < list_of_integers[size_semi + 1]:
+            if size_semi2 == 0:
+                size_semi2 = 2
+            size_semi += size_semi2 // 2
+
+        elif size_semi2 > 0 and list_of_integers[size_semi] < list_of_integers[size_semi - 1]:
+            if size_semi2 == 0:
+                size_semi2 = 2
+            size_semi -= size_semi2 // 2
+        else:
+            return list_of_integers[size_semi]
