7a97b9025e82388216c574754e281a7126484e06
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-11-02 09:28:49 -0700

    task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,26 +4,26 @@
 def find_peak(list_of_integers):
     """
     Args:
-        list_of_integers(int): list of integers
+        list_of_integers(int): list of integers to find peak of
     Returns: peak of list_of_integers or None
     """
     size = len(list_of_integers)
     mid_e = size
-    mids = size // 2
+    mid = size // 2
 
     if size == 0:
         return None
 
     while True:
         mid_e = mid_e // 2
-        if (mids < size - 1 and
-                list_of_integers[mids] < list_of_integers[mids + 1]):
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
             if mid_e // 2 == 0:
                 mid_e = 2
-            mids = mids + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mids] < list_of_integers[mids - 1]:
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
             if mid_e // 2 == 0:
                 mid_e = 2
-            mids = mids - mid_e // 2
+            mid = mid - mid_e // 2
         else:
-            return list_of_integers[mids]
+            return list_of_integers[mid]
