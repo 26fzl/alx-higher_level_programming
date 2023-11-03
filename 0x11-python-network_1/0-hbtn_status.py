86d515f4ddd45ee00ff00ec8f4e675f72d30e1e1
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-11-03 06:39:15 -0700

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 """script fetching https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-
 if __name__ == "__main__":
-    reqs = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(reqs) as resp:
-        body = resp.read()
-        print("Body resp:")
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
