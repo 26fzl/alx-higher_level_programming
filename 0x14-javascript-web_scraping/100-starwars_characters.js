a149aefa20cb56ee1902bc145242673836d8688a
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-11-28 04:15:10 -0800

    tasks

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (erro, respo, body) {	
+request(url, function (erro, respo, body) {
   if (!erro) {
     const characters = JSON.parse(body).characters;
     characters.forEach((character) => {
